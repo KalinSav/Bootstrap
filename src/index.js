1b4927e60cb53168239df4892dfe3ea88ddbcc03
KalinSav <48828687+KalinSav@users.noreply.github.com>
2019-08-14 12:48:13 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import $ from "jquery";
+
+$(window).scroll(function() {
+  $(".slideanim").each(function() {
+    var pos = $(this).offset().top;
+
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 600) {
+      $(this).addClass("slide");
+    }
+  });
+});
+
+// $(document).ready(function() {
+//   $(".carousel").carousel({
+//     interval: 2000
+//   });
+// });
